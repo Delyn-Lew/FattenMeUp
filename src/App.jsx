89d4f75dd741c4513eef124380a868de8974ef39
delynlpy@gmail.com <delynlpy@gmail.com>
2024-04-22 21:07:37 +0800

    vercel fetch test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-
-function App() {
-
-  return (
-    <>  
-    </>
-  )
+async function logMovies() {
+  const response = await fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=0cacfa8a8c0845328430f3573171b10e");
+  const movies = await response.json();
+  console.log(movies);
 }
 
-export default App
+function App(){
+  logMovies();
+  return(
+    <h1>test</h1>
+  )
+}
+export default App;
