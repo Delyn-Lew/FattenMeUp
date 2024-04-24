d570538b50cb5cbfef955525bfbe31cc8e632bb1
delynlpy@gmail.com <delynlpy@gmail.com>
2024-04-24 17:32:54 +0800

    click image of random Recipe will give the details of the recipe

--- src/components/RandomRecipes.jsx
+++ src/components/RandomRecipes.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 function RandomRecipes(){
     const [randomRecipes, setRandomRecipes] = useState([]);
     const [shouldFetch, setShouldFetch] = useState(true);
+    const [selectedRecipe, setSelectedRecipe] = useState(null);
 
     async function loadRandomRecipe(){
         const url = `https://api.spoonacular.com/recipes/random?number=1&apiKey=0cacfa8a8c0845328430f3573171b10e`;
@@ -20,16 +21,36 @@ function RandomRecipes(){
     function handleRandom() {
         loadRandomRecipe();
     }
+    
+    function handleRecipeClick(recipeId){
+        async function fetchRecipeDetails(){
+            const url = `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=0cacfa8a8c0845328430f3573171b10e`;
+            const response = await fetch(url);
+            const recipeDetails = await response.json();
+            setSelectedRecipe(recipeDetails);
+        }
+        fetchRecipeDetails();
+    }
     return(
         <>
-        <button onClick={handleRandom}>Randomise</button>
+        <button onClick={handleRandom}>Inspire me!</button>
         <div>
             {randomRecipes.map(recipe =>(
                 <div key={recipe.id}>
                     <h2>{recipe.title}</h2>
-                <img src={recipe.image} alt={recipe.title}></img>
+                    <img 
+                    src={recipe.image} 
+                    alt={recipe.title}
+                    onClick={()=> handleRecipeClick(recipe.id)}
+                    />
                 </div>
             ))}
+            {selectedRecipe && (
+                <div>
+                    <p>Ready in:{selectedRecipe.readyInMinutes}</p>
+                    <p>{selectedRecipe.summary}</p>
+                </div>
+            )}
         </div>
         </>
     );
